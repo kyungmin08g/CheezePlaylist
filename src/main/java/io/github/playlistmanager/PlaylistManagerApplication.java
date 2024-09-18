a3309bfb15a5cefa9c01fcccde6ca4dbe7d43696
KyungMin <k.kyungmin2892@gmail.com>
2024-09-18 14:03:12 +0900

    project setting

---
+++ src/main/java/io/github/playlistmanager/PlaylistManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.playlistmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaylistManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/playlistmanager/PlaylistManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.playlistmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
